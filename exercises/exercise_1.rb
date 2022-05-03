15e16173e5c3087e056d4ac76dcd6920486b7acb
double-slide <93881358+double-slide@users.noreply.github.com>
2022-05-03 02:29:10 +0000

    Exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,20 +6,21 @@ puts "----------"
 
 @store1 = Store.create(
   annual_revenue: 300000,
-  men: true,
-  women: true,
+  mens_apparel: true,
+  womens_apparel: true,
   name: "Burnaby"
 )
 @store2 = Store.create(
   annual_revenue: 1260000,
-  men: false,
-  women: true,
+  mens_apparel: false,
+  womens_apparel: true,
   name: "Richmond"
 )
 @store3 = Store.create(
   annual_revenue: 190000,
-  men: true,
-  women: false,
+  mens_apparel: true,
+  womens_apparel: false,
   name: "Gastown"
 )
 
+puts Store.count
