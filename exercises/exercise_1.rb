2dc14427a97f7e7275edafb1b9287e04c0dd6fd2
double-slide <93881358+double-slide@users.noreply.github.com>
2022-05-03 02:21:57 +0000

    Initial commit for ex 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,23 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+
+@store1 = Store.create(
+  annual_revenue: 300000,
+  men: true,
+  women: true,
+  name: "Burnaby"
+)
+@store2 = Store.create(
+  annual_revenue: 1260000,
+  men: false,
+  women: true,
+  name: "Richmond"
+)
+@store3 = Store.create(
+  annual_revenue: 190000,
+  men: true,
+  women: false,
+  name: "Gastown"
+)
+
