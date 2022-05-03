34f85d8b29aff7f83420176689ccf994599acdd4
double-slide <93881358+double-slide@users.noreply.github.com>
2022-05-03 03:04:58 +0000

    Exercise 2 and 3 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,19 +4,19 @@ puts "Exercise 1"
 puts "----------"
 
 
-@store1 = Store.create(
+Store.create(
   annual_revenue: 300000,
   mens_apparel: true,
   womens_apparel: true,
   name: "Burnaby"
 )
-@store2 = Store.create(
+Store.create(
   annual_revenue: 1260000,
   mens_apparel: false,
   womens_apparel: true,
   name: "Richmond"
 )
-@store3 = Store.create(
+Store.create(
   annual_revenue: 190000,
   mens_apparel: true,
   womens_apparel: false,
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -4,5 +4,10 @@ require_relative './exercise_2'
 
 puts "Exercise 3"
 puts "----------"
+puts Store.count
 
-# Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+@store3.destroy
+
+puts Store.count
